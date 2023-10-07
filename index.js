67f0e6640f48e76a075e03c49ea2fb74dbd3a07d
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-10-06 18:31:04 -0700

    Add custom account contract that can verify a webauthn ed25519 sig (#1)

--- index.js
+++ index.js
@@ -46,16 +46,17 @@
           name: "test1",
           displayName: "Test 1",
         },
-        pubKeyCredParams: [{ alg: -7, type: "public-key" }],
+        pubKeyCredParams: [{ alg: -8, type: "public-key" }],
       },
     });
     const response = credObj.response;
     const pk = response.getPublicKey();
+    const clientDataJSON = response.clientDataJSON;
+    const authenticatorData = response.authenticatorData;
+    const signature = response.signature;
 
     console.log("cred", {
-      pk: btoa(
-        String.fromCharCode.apply(null, new Uint8Array(pk)),
-      ),
+      pk: new Uint8Array(pk).join(","),
     });
     cred = credObj;
   }
@@ -64,7 +65,7 @@
     console.log("authing passkey");
     const authObj = await navigator.credentials.get({
       publicKey: {
-        challenge: new TextEncoder().encode("authchallenge000"),
+        challenge: new TextEncoder().encode("\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"),
         rpId: "localhost",
         allowCredentials: [{ type: "public-key", id: cred.rawId }],
       },
@@ -75,17 +76,9 @@
     const signature = response.signature;
 
     console.log("auth", {
-      clientDataJSON,
-      clientDataJSONBytes: btoa(
-        String.fromCharCode.apply(null, new Uint8Array(clientDataJSON)),
-      ),
-      authenticatorData,
-      authenticatorDataBytes: btoa(
-        String.fromCharCode.apply(null, new Uint8Array(authenticatorData)),
-      ),
-      signature: btoa(
-        String.fromCharCode.apply(null, new Uint8Array(signature)),
-      ),
+      authenticatorData: new Uint8Array(authenticatorData).join(","),
+      clientDataJSON: new Uint8Array(clientDataJSON).join(","),
+      signature: new Uint8Array(signature).join(","),
     });
   }
 
